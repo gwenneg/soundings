0b1436f71a6a2071d27858446ac18927003462fb
Gwenneg Lepage <gwenneg@users.noreply.github.com>
2025-12-17 14:26:56 +0100

    Fix issues from Claude's review (#50)

--- internal/git/gitlab/client.go
+++ internal/git/gitlab/client.go
@@ -1,29 +1,18 @@
 package gitlab
 
 import (
-	"fmt"
-
 	"gitlab.com/gitlab-org/api/client-go"
 	"release-confidence-score/internal/config"
 	httputil "release-confidence-score/internal/http"
 )
 
-func NewClient(cfg *config.Config) *gitlab.Client {
-	var client *gitlab.Client
-	var err error
-
+func NewClient(cfg *config.Config) (*gitlab.Client, error) {
 	if cfg.GitLabSkipSSLVerify {
 		httpClient := httputil.NewHTTPClient(httputil.HTTPClientOptions{
 			SkipSSLVerify: true,
 		})
-		client, err = gitlab.NewClient(cfg.GitLabToken, gitlab.WithBaseURL(cfg.GitLabBaseURL), gitlab.WithHTTPClient(httpClient))
-	} else {
-		client, err = gitlab.NewClient(cfg.GitLabToken, gitlab.WithBaseURL(cfg.GitLabBaseURL))
+		return gitlab.NewClient(cfg.GitLabToken, gitlab.WithBaseURL(cfg.GitLabBaseURL), gitlab.WithHTTPClient(httpClient))
 	}
 
-	if err != nil {
-		panic(fmt.Sprintf("Failed to create GitLab client: %v", err))
-	}
-
-	return client
+	return gitlab.NewClient(cfg.GitLabToken, gitlab.WithBaseURL(cfg.GitLabBaseURL))
 }
